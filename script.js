0eb881e241b654ade0a52cba8b7cfb9224c68a1f
Dominic Obeng Koranteng <nanaobeng087@gmail.com>
2025-07-14 22:56:58 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function displayMessage() {
+    document.getElementById('message').innerText = 'Hello, GitHub Pages!';
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+}
